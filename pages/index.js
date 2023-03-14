08b8119fbf5b7e0abd7eb8b8e3a1d98b174504c4
quechulo <@>
2023-03-14 13:54:39 +0100

    starting section 6

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,14 @@
-import fs from "fs/promises";
-import path from "path";
-import Link from "next/link";
+import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/event-list';
 
-function HomePage(props) {
-  const { products } = props;
+function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
-    <ul>
-      {products.map((product) => (
-        <li key={product.id}><Link href={`/products/${product.id}`}>{product.title}</Link></li>
-      ))}
-    </ul>
+    <div>
+      <EventList items={featuredEvents} />
+    </div>
   );
-}
-
-export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
-
-  return {
-    props: {
-      products: data.products,
-    },
-    revalidate: 10,
-  };
 }
 
 export default HomePage;
