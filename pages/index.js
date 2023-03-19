6182936028fcfd8d28e8b2f7e62674b88af75651
quechulo <@>
2023-03-19 22:50:25 +0100

    started Section 8

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,9 @@
-import Head from "next/head";
-import { getFeaturedEvents } from "../helpers/api-util";
-import EventList from "../components/events/event-list";
-
-function HomePage(props) {
+function HomePage() {
   return (
     <div>
-      <Head>
-        <title>NextJS Events</title>
-        <meta name="description" content="Find a lot of great events" />
-      </Head>
-      <EventList items={props.events} />
+      <h1>The Home Page</h1>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const featuredEvents = await getFeaturedEvents();
-
-  return {
-    props: {
-      events: featuredEvents,
-    },
-    revalidate: 1800,
-  };
 }
 
 export default HomePage;
