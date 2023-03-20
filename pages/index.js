fb27ff9d0a5146249236f9d5ac8d3145222ac36f
quechulo <@>
2023-03-20 18:16:31 +0100

    started Section 9

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,34 @@
-import { useRef, useState } from "react";
+import Head from 'next/head';
 
-function HomePage() {
-  const emailInputRef = useRef();
-  const feedbackInputRef = useRef();
-  const [allFeedback, setAllFeedback] = useState([]);
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
+import NewsletterRegistration from '../components/input/newsletter-registration';
 
-  const submitFormHandler = (event) => {
-    event.preventDefault();
-    const enteredEmail = emailInputRef.current.value;
-    const enteredFeedback = feedbackInputRef.current.value;
-
-    // {email: 'eldorado@op.pl', text: 'You make good job'}
-    fetch("/api/feedback", {
-      method: "POST", // or 'PUT'
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email: enteredEmail, text: enteredFeedback }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Success from index:", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  };
-
-  const loadAllFeedbackHandler = () => {
-    fetch("/api/feedback")
-      .then((response) => response.json())
-      .then((data) => {
-        setAllFeedback(data.feedback);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  };
-
+function HomePage(props) {
   return (
     <div>
-      <h1>The Home Page</h1>
-      <form onSubmit={submitFormHandler}>
-        <div>
-          <label htmlFor="email">Your email address</label>
-          <input type="email" id="email" ref={emailInputRef} />
-        </div>
-        <div>
-          <label htmlFor="feedback">Your feedback</label>
-          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
-        </div>
-        <button>Send Feedback</button>
-      </form>
-      <hr />
-      <button onClick={loadAllFeedbackHandler}>Load all feedback</button>
-      <ul>
-        {allFeedback.map((feedback) => (
-          <li key={feedback.id}>
-            { feedback.feedback }
-          </li>
-        ))}
-      </ul>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
+      <NewsletterRegistration />
+      <EventList items={props.events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
 }
 
 export default HomePage;
